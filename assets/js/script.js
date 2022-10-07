24a54634017dade79f90ca5b785aacf5692d13e8
Mike Fomin <mikefomin@Mikes-MacBook-Pro.local>
2022-10-06 17:05:04 -0700

    added the rest of the JS code so the the password generator can generate a password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
 // Assignment code here
 
 
+var passwordArea = document.getElementById('password')
+
+
+var characters = {
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  numbers: '0123456789',
+  special: "!@#$%^&*()_+~\\`|}{[]:;?><,./-="
+}
+
+
+var generateBtn = document.querySelector("#generate");
+var passwordText = document.querySelector("#password");
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var passwordString = ''
 
   var charactersLength = window.prompt('Select amount of characters desired for your password:')
 
@@ -21,15 +37,38 @@ function writePassword() {
 
   var specialConfirm = window.confirm('Would you like your password to include special characters?')
 
+  if (uppercaseConfirm === false && lowercaseConfirm === false && numbersConfirm === false && specialConfirm === false) {
+    window.alert('Please select OK on one of the given choices to generate password!')
+  }
+
+  if (uppercaseConfirm === true) {
+    passwordString = passwordString.concat(characters.uppercase)
+  }
+
+  if (lowercaseConfirm === true) {
+    passwordString = passwordString.concat(characters.lowercase)
+  }
+
+  if (numbersConfirm === true) {
+    passwordString = passwordString.concat(characters.numbers)
+  }
+
+  if (specialConfirm === true) {
+    passwordString = passwordString.concat(characters.special)
+  }
+
+  var passwordOptions = passwordString.split('')
+
+  var password = []
 
 
-
-
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  for (var index = 0; index < charactersLength; index++) {
+    var randomNumber = Math.floor(Math.random() * passwordOptions.length)
+    var random = passwordOptions[randomNumber]
+    password.push(random)  
+  }
+  password = password.join('')
+  passwordArea.textContent = password
 
 }
 
